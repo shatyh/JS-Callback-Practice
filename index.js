aaadd98bfd30274f58b4407bf28ccb28ee3dfcd7
Tita Hernandez <tita@Titas-MacBook-Pro.local>
2022-08-14 18:04:37 -0700

    changes 1

--- index.js
+++ index.js
@@ -2,7 +2,90 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+function handleDirectionChange (direction) {
+    if (direction === null) {
+        character.src = 'assets/green-character/static.gif'
+    }
+    if (direction === 'west') {
+        character.src = 'assets/green-character/west.gif'
+    }
+    if (direction === 'north') {
+        character.src = 'assets/green-character/east.gif'
+    }
+    if (direction === 'south') {
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move (character).withArrowKeys(100, 250, handleDirectionChange)
+move (character).withArrowKeys (100, 250)
+function moveWithArrowKeys (left, bottom) {
+let direction = null;
+let x = 100;
+let y = 250;
+
+element.style.left = x + 'px'
+element.style.bottom = y + 'px'
+
+function moveCharacter () {
+if(direction === 'west') {
+    x = x - 1
+}
+if(direction === 'north') {
+    y = y + 1
+}
+if(direction === 'south') {
+    y = y - 1
+}
+if(direction === 'east') {
+    x = x + 1
+}
+element.style.left = x + 'px'
+element.style.bottom = y + 'px'
+} 
+setInterval (moveCharacter, 1)
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+document.addEventListener ('keyup', function (e) {
+    direction = null
+})
+}
+
+
+function move (element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates (left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+    function moveWithArrowKeys (left, bottom) {
+
+    }
+
+        return {
+            to: moveToCoordinates,
+            withArrowKeys: moveWithArrowKeys
+        }
+}
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
